7c6a2c816833869aa3d318d823a3804df3b1e176
Roman Ostrous <ostrous.roman@gmail.com>
2024-03-10 22:55:13 +0200

    fix

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,7 +3,7 @@ import './HomePage.scss';
 export const HomePage = () => {
   return (
     <>
-      <img className="home__img" src='assets/gallery/Vectours.png' alt="" />
+      <img className="home__img" src='' alt="" />
     </>
   );
 };
