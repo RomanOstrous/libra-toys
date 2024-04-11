a20810dbcfe5c1bae15ba343079c8d76e7a3645a
Roman Ostrous <ostrous.roman@gmail.com>
2024-04-11 14:38:27 +0300

    stylized the form of the new almost and added a request to create an order

--- src/components/NewPochta/NewPochta.tsx
+++ src/components/NewPochta/NewPochta.tsx
@@ -1,15 +1,27 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './NewPochta.scss';
 import { debounce } from 'lodash';
 import arrow from '../../assets/icons/arrow.svg';
 import axios from 'axios';
+import classNames from 'classnames';
 
 const NewPochta = () => {
-  const [phone, setPhone] = useState('');
-  const [city, setCity] = useState('');
-  const [warehouse, setWarehouse] = useState('');
+  const [phone, setPhone] = useState(sessionStorage.getItem('phone') || '');
+  const [city, setCity] = useState(sessionStorage.getItem('city') || '');
+  const [warehouse, setWarehouse] = useState(sessionStorage.getItem('warehouse') || '');
   const [optionsCity, setoptionsCity] = useState<string[]>([]);
   const [optionsWarehouse, setoptionsWarehouse] = useState<string[]>([]);
+  const [button, setButton] = useState(false);
+
+  const saveToSessionStorage = useCallback(() => {
+    sessionStorage.setItem('phone', phone);
+    sessionStorage.setItem('city', city);
+    sessionStorage.setItem('warehouse', warehouse);
+  }, [phone, city, warehouse]);
+
+  useEffect(() => {
+    saveToSessionStorage();
+  }, [saveToSessionStorage]);
 
   const debouncedSearchCity = useCallback(
     debounce(async (text: string) => {
@@ -93,83 +105,98 @@ const NewPochta = () => {
   };
 
   return (
-    <div className="buy__newp">
-      <button className="buy-form__np-select">
-        <p>Доставка Новою Поштою</p>
-        <img src={arrow} alt="" />
+    <div className="newp">
+      <button className="newp__select" onClick={() => setButton(!button)}>
+        <p className="newp__title">Доставка Новою Поштою</p>
+        <img 
+          src={arrow} alt=""
+          className={classNames("newp__arrow", {
+              "newp__arrow-active": button === false 
+            })} />
       </button>
+      
 
-      <p>
-        Номер телефону 
-        <span style={{color: '#DD2525'}}>*</span>
-      </p>
-      <input 
-        type="text" 
-        name="телефон" 
-        value={phone}
-        onChange={handleChangePhone}
-        placeholder='Введи свій телефон'
-      />
-
-      <div>
-        <div>
-          <p>
-            Місто
-            <span style={{color: '#DD2525'}}>*</span>
+        <div className={`newp__info ${button === true ? 'newp__info--visible' : ''}`}>
+          <div className="newp__box">
+            <p className="newp__text">
+              Номер телефону 
+              <span style={{color: '#DD2525'}}>*</span>
             </p>
-          <input
-            type="text"
-            value={city}
-            name="населений пункт" 
-            onChange={handleChangeCity}
-            placeholder="Введи своє місто"
-            onBlur={() => setTimeout(() => {
-              setoptionsCity([]);
-            }, 100)}
-          />
+            <input 
+              className="newp__input"
+              type="text" 
+              name="телефон" 
+              value={phone}
+              onChange={handleChangePhone}
+              placeholder='Введи свій телефон'
+              autoComplete='off'
+            />
+          </div>
 
-          <div className="options-container">
-            <ul className="options-list">
-              {optionsCity.map((option, index) => (
-                <li key={index} onClick={() => handleSelectCity(option)}>
-                  {option}
-                </li>
-              ))}
-            </ul>
+          <div className="newp__container">
+            <div className="newp__box">
+              <p className="newp__text">
+                Місто
+                <span style={{color: '#DD2525'}}>*</span>
+              </p>
+              <input
+                autoComplete='off'
+                className="newp__input"
+                type="text"
+                value={city}
+                name="населений пункт" 
+                onChange={handleChangeCity}
+                placeholder="Введи своє місто"
+                onBlur={() => setTimeout(() => {
+                  setoptionsCity([]);
+                }, 100)}
+              />
+
+              <div className="newp__options">
+                <ul className="newp__options-list">
+                  {optionsCity.map((option, index) => (
+                    <li className="newp__option" key={index} onClick={() => handleSelectCity(option)}>
+                      {option}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+            <div className="newp__box">
+              <p className="newp__text">
+                Відділення пошти 
+                <span style={{color: '#DD2525'}}>*</span>
+              </p>
+              <input
+                className="newp__input"
+                type="text"
+                value={warehouse}
+                name="відділення"
+                autoComplete='off' 
+                onChange={handleChangeWarehouse}
+                onFocus={handleChangeWarehouse}
+                placeholder="Введи своє відділення"
+                onBlur={() => setTimeout(() => {
+                  setoptionsWarehouse([]);
+                }, 100)}
+              />
+
+              <div className="newp__options">
+                <ul className="newp__options-list">
+                  {optionsWarehouse.map((option, index) => (
+                    <li className="newp__option" key={index} onClick={() => handleSelectWarehouse(option)}>
+                      {option}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
 
-        <div>
-          <p>
-            Відділення пошти 
-            <span style={{color: '#DD2525'}}>*</span>
-          </p>
-          <input
-            type="text"
-            value={warehouse}
-            name="відділення" 
-            onChange={handleChangeWarehouse}
-            onFocus={handleChangeWarehouse}
-            placeholder="Введи своє відділення"
-            onBlur={() => setTimeout(() => {
-              setoptionsWarehouse([]);
-            }, 100)}
-          />
-
-          <div className="options-container">
-            <ul className="options-list">
-              {optionsWarehouse.map((option, index) => (
-                <li key={index} onClick={() => handleSelectWarehouse(option)}>
-                  {option}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
     </div>
   )
 }
-
 
 export default NewPochta
--- src/pages/Buy/BuyPage.tsx
+++ src/pages/Buy/BuyPage.tsx
@@ -3,15 +3,64 @@ import './BuyPage.scss'
 
 import ButtonBack from '../../components/ButtonBack/ButtonBack';
 import NewPochta from '../../components/NewPochta/NewPochta';
+import { useAppSelector } from '../../app/hook';
+import { client } from '../../services/httpClient';
+import Cookies from 'js-cookie';
+import axios from 'axios';
 
 
 const BuyPage = () => {
+  const { product } = useAppSelector(state => state.product);
+  const { cart } = useAppSelector(state => state.cart);
+  const phone = sessionStorage.getItem('phone');
+  const city = sessionStorage.getItem('city');
+  const warehouse = sessionStorage.getItem('warehouse');
+  const token = Cookies.get('access_token');
+  const base = process.env.REACT_APP_BASE_URL;
+
+  const visibleToys = product.filter(item => cart.includes(item.id));
+  console.log("korzuha", cart);
+
+  const infoObj = visibleToys.map(el => ({
+    "product": el.id,
+    "price": el.price,
+  }));
+
+  console.log(infoObj);
+
+  const handleBuy = () => {
+    axios.post(base + 'user/orders/', {
+      "order_items": infoObj,
+      "delivery_city": city,
+      "delivery_warehouse": warehouse,
+    }, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    .then(response => {
+      console.log('все чотко', response)
+    });
+  }
 
   return (
     <div className="buy">
-      <ButtonBack text='Придбати'/>
+      <div className="buy__container container grid">
+        <div className="grid__item--desktop-1-4 grid__item--tablet-1-4">
+          <ButtonBack text='Придбати'/>
+        </div>
+        
+        <div className="grid__item--desktop-1-4 grid__item--tablet-1-4">
+          <NewPochta />
 
-      <NewPochta />
+          <button 
+            className="buy__button grid__item--desktop-1-4 grid__item--tablet-1-4"
+            onClick={() => handleBuy()}
+          >
+            Придбати
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
